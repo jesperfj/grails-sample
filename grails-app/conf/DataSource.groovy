004534825b74b0269ab9a2d9b1b8992ca3ab7b3e
Jesper Joergensen <jesper@heroku.com>
2011-09-08 15:57:19 -0700

    database config

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -26,7 +26,14 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+
+            url = "jdbc:postgresql://"+uri.host+uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
         }
     }
 }
