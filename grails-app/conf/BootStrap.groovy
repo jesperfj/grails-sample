3561091916fe7061bcbf230a3ac1d8db500ad566
Jesper Joergensen <jesper@heroku.com>
2011-09-08 15:51:47 -0700

    Complete tutorial app (before data source config)

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,15 @@
-class BootStrap {
+import org.example.Book
 
+class BootStrap {
     def init = { servletContext ->
+        // Check whether the test data already exists.
+        if (!Book.count()) {
+            new Book(author: "Stephen King", title: "The Shining").save(failOnError: true)
+            new Book(author: "James Patterson", title: "Along Came a Spider").save(failOnError: true)
+        }
     }
+
     def destroy = {
     }
 }
+
